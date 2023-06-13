88e7c20d819f8e6afecab94801a3281ff4eb7f86
DanielY1108 <scarlet040@gmail.com>
2023-06-13 16:50:09 +0900

    Initial Commit

---
+++ Alert-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alert-Demo
+//
+//  Created by JINSEOK on 2023/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
