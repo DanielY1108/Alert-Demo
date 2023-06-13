5f942de1660eebbf2b4392733b61ff321d61a1bf
DanielY1108 <scarlet040@gmail.com>
2023-06-13 19:10:57 +0900

    완성

--- Alert-Demo/ViewController.swift
+++ Alert-Demo/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var button: UIButton = {
+        
+        let button = UIButton(frame: CGRect(x: 150, y: 530, width: 100, height: 50),
+                              primaryAction: UIAction(handler: { [weak self] action in
+            self?.setupAlert()
+        }))
+        
+        var config = UIButton.Configuration.filled()
+        config.title = "Go Alert"
+        button.configuration = config
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(button)
     }
 
+    func setupAlert() {
+        let alert = UIAlertController(title: "로그인",
+                                      message: "ID와 Password를 입력해주세요.",
+                                      preferredStyle: .alert)
+        alert.view.tintColor = .red
+        alert.view.backgroundColor = .blue
+        alert.view.layer.cornerRadius = 16
+        alert.view.layer.masksToBounds = true
+
+        alert.addTextField { textField in
+            textField.placeholder = "ID를 입력해주세요."
+        }
+        
+        alert.addTextField { textField in
+            textField.placeholder = "Password를 입력해주세요."
+        }
+
+        let doneAction = UIAlertAction(title: "확인",
+                                       style: .default) { action in
+            // 동작을 설정
+        }
+    
+        let cancelAction = UIAlertAction(title: "취소",
+                                         style: .cancel) { action in
+            // 동작을 설정
+        }
+        
+        alert.addAction(doneAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true)
+    }
 
 }
+
+
 
